27b05e41ff4e0a1fec43929db1b2ffe439d28e37
ishan2790 <ishan.codework@gmail.com>
2021-11-28 15:55:43 +0530

    initial commit

---
+++ src/components/Question.svelte
@@ -0,0 +1,24 @@
+<script>
+    //import { CodeSnippet } from "carbon-components-svelte";
+    import {
+    Form,
+    FormGroup,
+    RadioButtonGroup,
+    RadioButton,
+  } from "carbon-components-svelte";    
+  export let question;
+  export let questionnum;
+  questionnum +=1;
+  let questionstring = "Q."+ questionnum +"   ";
+</script>
+
+
+<Form>
+    <FormGroup legendText={questionstring}{question.q}>
+        <RadioButtonGroup name={question.id} orientation="vertical">
+            {#each question.opt as option (option.id)}
+                <RadioButton labelText={option.a} value={option.a}></RadioButton>
+	        {/each}
+        </RadioButtonGroup>
+    </FormGroup>
+</Form>
---
+++ src/components/QuestionList.svelte
@@ -0,0 +1,34 @@
+<script>
+    import Question from './Question.svelte'
+    import {
+      Content,
+      Grid,
+      Row,
+      Column,
+    } from "carbon-components-svelte";
+    let questions = [{
+        id: '000001',
+        q: 'What is the capital of India',
+        opt: [{a:'Delhi',c: true,id:1},{a:'Kolkata',c:false,id:2}]
+    },
+    {
+        id: '000002',
+        q: 'What is the capital of Australia',
+        opt: [{a:'Canberra',c: true,id:1},{a:'Melbourne',c:false,id:2}]
+    }
+    ]
+     let questionnum
+     
+</script>
+
+<Content>
+    <Grid>
+        {#each questions as question, questionnum}     
+        <Row>
+            <Column>
+                    <Question {question} {questionnum}/>
+         </Column> 
+        </Row>
+        {/each}
+    </Grid>
+</Content>
